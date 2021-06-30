23cdfaa89b21869b7ae1acd395a3670de26bc38c
nehalkuchhal <66009922+nehalkuchhal@users.noreply.github.com>
2021-06-30 17:16:11 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,15 +1,24 @@
 from django.urls import path
-from .views import ApplicantDetailView , SchoolView, ExperienceView, HigherView, CourseView, ProjectView, SkillView, CompanyView, OpeningView, SelectionView
+from .views import ( 
+ApplicantDetailView,
+EducationTypeView,
+ExperienceView,
+CourseView,
+ProjectView,
+SkillView,
+CompanyView,
+OpeningView,
+SelectionView
+)
 
 urlpatterns = [
     path('', ApplicantDetailView.as_view()),
-    path('secondary/', SchoolView.as_view()),
-    path('higher/', HigherView.as_view()),
-    path('experience/', ExperienceView.as_view()),
-    path('course/', CourseView.as_view()),
-    path('project/', ProjectView.as_view()),
-    path('skill/', SkillView.as_view()),
-    path('company/', CompanyView.as_view()),
-    path('openings/', OpeningView.as_view()),
-    path('selection/', SelectionView.as_view()),
+    path('education', EducationTypeView.as_view()),
+    path('experience', ExperienceView.as_view()),
+    path('course', CourseView.as_view()),
+    path('project', ProjectView.as_view()),
+    path('skill', SkillView.as_view()),
+    path('company', CompanyView.as_view()),
+    path('openings', OpeningView.as_view()),
+    path('selection', SelectionView.as_view()),
 ]
